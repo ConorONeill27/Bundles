8941b0df66a25449d528bb890539286138b7a4d8
Conor <168658118+ConorONeill27@users.noreply.github.com>
2025-09-17 22:38:43 +0100

    manifest.js sprockets fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,12 @@
+// Link all image files under app/assets/images
 //= link_tree ../images
+
+// Link all CSS files under app/assets/stylesheets
 //= link_directory ../stylesheets .css
-//= link application.js
-// In app/assets/javascripts/application.js
-//= require graph
-//= link application.js
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+
+// Link legacy JavaScript files under app/assets/javascripts
+// Only include files here that are not part of app/javascript (Rails 7 importmaps/jsbundling)
+//= link_tree ../javascripts .js
+
+// Do NOT link application.js if you have app/javascript/application.js
+// Rails 7 handles that automatically via importmaps or jsbundling
